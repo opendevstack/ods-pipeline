9234edb6b06390c6a3f0563c3ca736dcedc3fe07
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2021-06-01 10:38:19 +0200

    Fix Bitbucket setup

--- scripts/run-bitbucket.sh
+++ scripts/run-bitbucket.sh
@@ -1,21 +1,43 @@
 #!/usr/bin/env bash
 set -ue
 
+# Starts a Bitbucket instance with a timebomb license (3 hours).
+# The instance is setup with an admin account (pw: admin) and an "ODSPIPELINETEST" project.
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ODS_PIPELINE_DIR=${SCRIPT_DIR%/*}
 
 INSECURE=""
-HOST_PORT="7990"
-CONTAINER_NAME="bitbuckettest"
-BITBUCKET_IMAGE_TAG="latest"
+BITBUCKET_SERVER_HOST_PORT="7990"
+BITBUCKET_SERVER_CONTAINER_NAME="bitbucket-server"
+BITBUCKET_SERVER_IMAGE_TAG="latest"
+BITBUCKET_POSTGRES_HOST_PORT="7990"
+BITBUCKET_POSTGRES_CONTAINER_NAME="bitbucket-postgres"
+BITBUCKET_POSTGRES_IMAGE_TAG="12"
 K8S_SECRET_FILE="${ODS_PIPELINE_DIR}/test/testdata/deploy/cd-kind/secret-bitbucket-auth.yml"
 K8S_CONFIGMAP_FILE="${ODS_PIPELINE_DIR}/test/testdata/deploy/cd-kind/configmap-bitbucket.yml"
 
-echo "Run container using image tag ${BITBUCKET_IMAGE_TAG}"
-docker rm -f ${CONTAINER_NAME} || true
-docker run -d --name ${CONTAINER_NAME} -e ELASTICSEARCH_ENABLED=false --net kind -p "${HOST_PORT}:7990" -p 7999:7999 atlassian/bitbucket-server:${BITBUCKET_IMAGE_TAG}
 
-BITBUCKET_URL="http://localhost:${HOST_PORT}"
+echo "Run Postgres container"
+docker rm -f ${BITBUCKET_POSTGRES_CONTAINER_NAME} || true
+docker run  --name ${BITBUCKET_POSTGRES_CONTAINER_NAME} \
+  -v ${ODS_PIPELINE_DIR}/testdata/bitbucket.sql:/docker-entrypoint-initdb.d/init.sql \
+  -e POSTGRES_PASSWORD=jellyfish -e POSTGRES_USER=bitbucketuser -e POSTGRES_DB=bitbucket \
+  -d --net kind -p "${BITBUCKET_POSTGRES_HOST_PORT}:5432" \
+  postgres:${BITBUCKET_POSTGRES_IMAGE_TAG}
+
+echo "Run Bitbucket Server pointing to Postgres"
+docker rm -f ${BITBUCKET_SERVER_CONTAINER_NAME} || true
+docker run --name ${BITBUCKET_SERVER_CONTAINER_NAME} \
+  -e JDBC_DRIVER=org.postgresql.Driver \
+  -e JDBC_USER=bitbucketuser \
+  -e JDBC_PASSWORD=jellyfish \
+  -e JDBC_URL=jdbc:postgresql://${BITBUCKET_POSTGRES_CONTAINER_NAME}.kind:5432/bitbucket \
+  -e ELASTICSEARCH_ENABLED=false \
+  -d --net kind -p "${BITBUCKET_SERVER_HOST_PORT}:7990" -p 7999:7999 \
+  atlassian/bitbucket-server:${BITBUCKET_SERVER_IMAGE_TAG}
+
+BITBUCKET_URL="http://localhost:${BITBUCKET_SERVER_HOST_PORT}"
 echo "Waiting up to 3 minutes for Bitbucket to start ..."
 # https://confluence.atlassian.com/bitbucketserverkb/how-to-monitor-if-bitbucket-server-is-up-and-running-975014635.html
 n=0
@@ -38,10 +60,11 @@ until [ $n -ge 18 ]; do
 done
 set -e
 
+# Personal access token (PAT) is baked into the SQL dump.
 cat <<EOF >${K8S_SECRET_FILE}
 apiVersion: v1
 stringData:
-  password: create_access_token_and_enter_here
+  password: NzU0OTk1MjU0NjEzOpzj5hmFNAaawvupxPKpcJlsfNgP
   username: admin
 kind: Secret
 metadata:
@@ -55,8 +78,5 @@ apiVersion: v1
 metadata:
   name: bitbucket
 data:
-  url: 'http://${CONTAINER_NAME}.kind:7999'
+  url: 'http://${BITBUCKET_SERVER_CONTAINER_NAME}.kind:7999'
 EOF
-
-echo "If setup is needed, visit ${BITBUCKET_URL} and create an access token and a project 'FOO'."
-echo "Use timebomb license from https://developer.atlassian.com/platform/marketplace/timebomb-licenses-for-testing-server-apps/ (3 hour expiration for all Atlassian host products)"
