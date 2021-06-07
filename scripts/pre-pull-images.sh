b1ccb6a209ca4393d989b34ac24cbd1af92917f4
zxBCN Cruz_Martinez <felipe.cruz_martinez.ext@boehringer-ingelheim.com>
2021-06-07 13:25:46 +0200

    Check if kubefledge is already installed and wait for deployments to be ready

--- scripts/pre-pull-images.sh
+++ scripts/pre-pull-images.sh
@@ -7,18 +7,31 @@ set -ue
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ODS_PIPELINE_DIR=${SCRIPT_DIR%/*}
+KUBE_CONTEXT="--context kind-kind"
+KUBECTL_BIN="kubectl $KUBE_CONTEXT"
 KUBEFLEDGED_VERSION="v0.7.1"
 KUBE_FLEDGED_FOLDER="${ODS_PIPELINE_DIR}/scripts/kube-fledged"
 
-echo $KUBE_FLEDGED_FOLDER
-
 # Install kube-fledged
-if [ ! -d "$KUBE_FLEDGED_FOLDER" ] ; then
-    git clone --depth 1 https://github.com/senthilrch/kube-fledged.git --branch ${KUBEFLEDGED_VERSION}
+if ! $KUBECTL_BIN get namespace kube-fledged &> /dev/null; then
+
+    echo "Installing kube-fledged ${KUBEFLEDGED_VERSION} ..."
+    if [ ! -d "$KUBE_FLEDGED_FOLDER" ] ; then
+        git clone --depth 1 https://github.com/senthilrch/kube-fledged.git --branch ${KUBEFLEDGED_VERSION}
+    fi
+
+    echo "kube-fledge repo already exists. Using local directory."
     cd kube-fledged/ && make deploy-using-yaml
+
+    # Wait for kube-fledge deployments to be ready
+    $KUBECTL_BIN rollout status deploy/kubefledged-controller -n kube-fledged
+    $KUBECTL_BIN rollout status deploy/kubefledged-webhook-server -n kube-fledged
 fi
+
+echo "kube-fledge already installed."
 
 # Apply ImageCache Resource with the list of ODS images to pre-pull.
 # Edit it as per your needs before creating image cache.
 # If images are in private repositories requiring credentials to pull, add "imagePullSecrets" to the end.
-kubectl apply -f "${ODS_PIPELINE_DIR}/scripts/deploy/kubefledged-imagecache-ods-images.yaml"
+echo "Applying ImageCache resourece for ODS images..."
+$KUBECTL_BIN apply -f "${ODS_PIPELINE_DIR}/scripts/deploy/kubefledged-imagecache-ods-images.yaml"
