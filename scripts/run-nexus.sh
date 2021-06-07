a29bec047e5aa944ff6cc5e2be0c1227ee0ae1c7
zxBCN Cruz_Martinez <felipe.cruz_martinez.ext@boehringer-ingelheim.com>
2021-06-07 14:03:49 +0200

    Increase Nexus startup timeout to 6 min

--- scripts/run-nexus.sh
+++ scripts/run-nexus.sh
@@ -32,11 +32,11 @@ docker run -d -p "${HOST_PORT}:8081" --net kind --name ${CONTAINER_NAME} sonatyp
 
 NEXUS_URL="http://localhost:${HOST_PORT}"
 function waitForReady {
-    echo "Waiting up to 3 minutes for Nexus to start ..."
+    echo "Waiting up to 6 minutes for Nexus to start ..."
     local n=0
     local http_code=
     set +e
-    until [ $n -ge 18 ]; do
+    until [ $n -ge 36 ]; do
         http_code=$(curl ${INSECURE} -s -o /dev/null -w "%{http_code}" "${NEXUS_URL}/service/rest/v1/status/writable")
         if [ "${http_code}" == "200" ]; then
             echo " success"
