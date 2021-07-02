40c6934421c268616816cfaca9682d314209e07b
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2021-07-02 15:42:27 +0200

    create release namespace

--- test/tasks/ods-deploy-helm_test.go
+++ test/tasks/ods-deploy-helm_test.go
@@ -1,15 +1,20 @@
 package tasks
 
 import (
+	"context"
 	"io/ioutil"
 	"path/filepath"
 	"testing"
 
-	"github.com/opendevstack/pipeline/internal/kubernetes"
+	k "github.com/opendevstack/pipeline/internal/kubernetes"
 	"github.com/opendevstack/pipeline/internal/random"
 	"github.com/opendevstack/pipeline/pkg/config"
 	"github.com/opendevstack/pipeline/pkg/tasktesting"
 	"sigs.k8s.io/yaml"
+
+	v1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 func TestTaskODSDeployHelm(t *testing.T) {
@@ -26,31 +31,18 @@ func TestTaskODSDeployHelm(t *testing.T) {
 					}
 
 					// TODO: defer cleanup
-					releaseNamespace := random.PseudoString()
-					kubernetes.CreateNamespace(ctxt.Clients.KubernetesClientSet, releaseNamespace)
+					releaseNamespace, err := createReleaseNamespace(
+						ctxt.Clients.KubernetesClientSet,
+						ctxt.Namespace,
+					)
+					if err != nil {
+						t.Fatal(err)
+					}
 
-					o := &config.ODS{
-						Environments: config.Environments{
-							DEV: config.Environment{
-								Targets: []config.Target{
-									{
-										Name:      "default",
-										Namespace: releaseNamespace,
-									},
-								},
-							},
-						},
-					}
-					y, err := yaml.Marshal(o)
+					err = createODSYML(wsDir, releaseNamespace)
 					if err != nil {
 						t.Fatal(err)
 					}
-					filename := filepath.Join(wsDir, "ods.yml")
-					err = ioutil.WriteFile(filename, y, 0644)
-					if err != nil {
-						t.Fatal(err)
-					}
-					//k.CreateNamespace(c.KubernetesClientSet, "michael-dev")
 				},
 				WantRunSuccess: true,
 				PostRunFunc: func(t *testing.T, ctxt *tasktesting.TaskRunContext) {
@@ -59,4 +51,53 @@ func TestTaskODSDeployHelm(t *testing.T) {
 			},
 		},
 	)
+}
+
+func createReleaseNamespace(clientset *kubernetes.Clientset, ctxtNamespace string) (string, error) {
+	releaseNamespace := random.PseudoString()
+	k.CreateNamespace(clientset, releaseNamespace)
+	_, err := clientset.RbacV1().RoleBindings(releaseNamespace).Create(
+		context.Background(),
+		&v1.RoleBinding{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "pipeline-deployer",
+				Namespace: releaseNamespace,
+			},
+			Subjects: []v1.Subject{
+				{
+					Kind:      "ServiceAccount",
+					Name:      "pipeline",
+					Namespace: ctxtNamespace,
+				},
+			},
+			RoleRef: v1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Kind:     "ClusterRole",
+				Name:     "edit",
+			},
+		},
+		metav1.CreateOptions{})
+
+	return releaseNamespace, err
+}
+
+func createODSYML(wsDir, releaseNamespace string) error {
+	o := &config.ODS{
+		Environments: config.Environments{
+			DEV: config.Environment{
+				Targets: []config.Target{
+					{
+						Name:      "default",
+						Namespace: releaseNamespace,
+					},
+				},
+			},
+		},
+	}
+	y, err := yaml.Marshal(o)
+	if err != nil {
+		return err
+	}
+	filename := filepath.Join(wsDir, "ods.yml")
+	return ioutil.WriteFile(filename, y, 0644)
 }
