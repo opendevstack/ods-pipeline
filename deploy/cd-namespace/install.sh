112646bcf04e8116a2b5af5394df101e7c8069ed
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2021-08-05 08:47:40 +0200

    Start user guide

---
+++ deploy/cd-namespace/install.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+set -ue
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+VERBOSE="false"
+DIFF="true"
+NAMESPACE=""
+RELEASE_NAME="ods-pipeline"
+SERVICEACCOUNT="pipeline"
+VALUES_FILE="values.custom.yaml"
+CHART_DIR="./chart"
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+
+    -v|--verbose) VERBOSE="true";;
+
+    -n|--namespace) NAMESPACE="$2"; shift;;
+    -n=*|--namespace=*) NAMESPACE="${1#*=}";;
+
+    -f|--values) VALUES_FILE="$2"; shift;;
+    -f=*|--values=*) VALUES_FILE="${1#*=}";;
+
+    -s|--serviceaccount) SERVICEACCOUNT="$2"; shift;;
+    -s=*|--serviceaccount=*) SERVICEACCOUNT="${1#*=}";;
+
+    --no-diff) DIFF="false"; shift;;
+
+    *) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
+
+cd ${SCRIPT_DIR}
+
+VALUES_FILES=$(echo $VALUES_FILE | tr "," "\n")
+VALUES_ARGS=""
+for valueFile in ${VALUES_FILES}; do
+    VALUES_ARGS="${VALUES_ARGS} --values=${CHART_DIR}/${valueFile}"
+done
+
+if [ "${VERBOSE}" == "true" ]; then
+    set -x
+fi
+
+if kubectl -n ${NAMESPACE} get serviceaccount/${SERVICEACCOUNT} &> /dev/null; then
+    echo "Serviceaccount exists already ..."
+else
+    echo "Creating serviceaccount ..."
+    kubectl -n ${NAMESPACE} create serviceaccount ${SERVICEACCOUNT}
+
+    kubectl -n ${NAMESPACE} \
+        create rolebinding "${SERVICEACCOUNT}-edit" \
+        --clusterrole edit \
+        --serviceaccount "${NAMESPACE}:${SERVICEACCOUNT}"
+fi
+
+echo "Installing Helm release ..."
+if [ "${DIFF}" == "true" ]; then
+    if helm -n ${NAMESPACE} \
+            diff upgrade --install --detailed-exitcode \
+            ${VALUES_ARGS} \
+            ${RELEASE_NAME} ${CHART_DIR}; then
+        echo "Helm release already up-to-date."
+    else
+        helm -n ${NAMESPACE} \
+            upgrade --install \
+            ${VALUES_ARGS} \
+            ${RELEASE_NAME} ${CHART_DIR}
+    fi
+else
+    helm -n ${NAMESPACE} \
+            upgrade --install \
+            ${VALUES_ARGS} \
+            ${RELEASE_NAME} ${CHART_DIR}
+fi
+
+echo "Adding ods-bitbucket-auth secret to serviceaccount ..."
+kubectl -n ${NAMESPACE} \
+    patch sa ${SERVICEACCOUNT} \
+    --type json \
+    -p '[{"op": "add", "path": "/secrets", "value":[{"name": "ods-bitbucket-auth"}]}]'
+
+# echo "Exposing event listener ..."
+# oc -n ${NAMESPACE} expose svc el-ods-pipeline
--- scripts/install-cd-namespace-resources.sh
+++ scripts/install-cd-namespace-resources.sh
@@ -4,82 +4,9 @@ set -ue
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ODS_PIPELINE_DIR=${SCRIPT_DIR%/*}
 
-VERBOSE="false"
-DIFF="true"
-NAMESPACE=""
-RELEASE_NAME="ods-pipeline"
-SERVICEACCOUNT="pipeline"
-VALUES_FILE="values.custom.yaml"
-CHART_DIR="./chart"
-
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-
-    -v|--verbose) VERBOSE="true";;
-
-    -n|--namespace) NAMESPACE="$2"; shift;;
-    -n=*|--namespace=*) NAMESPACE="${1#*=}";;
-
-    -f|--values) VALUES_FILE="$2"; shift;;
-    -f=*|--values=*) VALUES_FILE="${1#*=}";;
-
-    -s|--serviceaccount) SERVICEACCOUNT="$2"; shift;;
-    -s=*|--serviceaccount=*) SERVICEACCOUNT="${1#*=}";;
-
-    --no-diff) DIFF="false"; shift;;
-
-    *) echo "Unknown parameter passed: $1"; exit 1;;
-esac; shift; done
-
-cd ${ODS_PIPELINE_DIR}/deploy/cd-namespace
-
-VALUES_FILES=$(echo $VALUES_FILE | tr "," "\n")
-VALUES_ARGS=""
-for valueFile in ${VALUES_FILES}; do
-    VALUES_ARGS="${VALUES_ARGS} --values=${CHART_DIR}/${valueFile}"
-done
-
-if [ "${VERBOSE}" == "true" ]; then
-    set -x
-fi
-
-if kubectl -n ${NAMESPACE} get serviceaccount/${SERVICEACCOUNT} &> /dev/null; then
-    echo "Serviceaccount exists already ..."
-else
-    echo "Creating serviceaccount ..."
-    kubectl -n ${NAMESPACE} create serviceaccount ${SERVICEACCOUNT}
-
-    kubectl -n ${NAMESPACE} \
-        create rolebinding "${SERVICEACCOUNT}-edit" \
-        --clusterrole edit \
-        --serviceaccount "${NAMESPACE}:${SERVICEACCOUNT}"
-fi
-
-echo "Installing Helm release ..."
-if [ "${DIFF}" == "true" ]; then
-    if helm -n ${NAMESPACE} \
-            diff upgrade --install --detailed-exitcode \
-            ${VALUES_ARGS} \
-            ${RELEASE_NAME} ${CHART_DIR}; then
-        echo "Helm release already up-to-date."
-    else
-        helm -n ${NAMESPACE} \
-            upgrade --install \
-            ${VALUES_ARGS} \
-            ${RELEASE_NAME} ${CHART_DIR}
-    fi
-else
-    helm -n ${NAMESPACE} \
-            upgrade --install \
-            ${VALUES_ARGS} \
-            ${RELEASE_NAME} ${CHART_DIR}
-fi
-
-echo "Adding ods-bitbucket-auth secret to serviceaccount ..."
-kubectl -n ${NAMESPACE} \
-    patch sa ${SERVICEACCOUNT} \
-    --type json \
-    -p '[{"op": "add", "path": "/secrets", "value":[{"name": "ods-bitbucket-auth"}]}]'
-
-# echo "Exposing event listener ..."
-# oc -n ${NAMESPACE} expose svc el-ods-pipeline
+# Delegate to install.sh within deploy/cd-namespace.
+# The script here exists only for consistency (all scripts are located under /scripts)
+# and the install.sh script is under deploy/cd-namespace so that the whole
+# deployment is self-contained within that folder, making it easy for consumers
+# to pull in the deployment logic into their repositories via "git subtree".
+${ODS_PIPELINE_DIR}/deploy/cd-namespace/install.sh "$@"
