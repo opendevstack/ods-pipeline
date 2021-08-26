76730cf2937567bd8ce6d9eb20ec10ae1bb9dd27
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2021-08-26 10:50:11 +0200

    Update Tekton version

--- scripts/install-tekton-pipelines.sh
+++ scripts/install-tekton-pipelines.sh
@@ -4,11 +4,11 @@ set -eu
 KUBE_CONTEXT="--context kind-kind"
 KUBECTL_BIN="kubectl $KUBE_CONTEXT"
 
-# Tekton versions are aligned with Red Hat OpenShift Pipelines General Availability 1.4.
-# See https://docs.openshift.com/container-platform/4.7/cicd/pipelines/op-release-notes.html.
-TKN_VERSION="v0.22.0"
+# Tekton versions are aligned with Red Hat OpenShift Pipelines General Availability 1.5.
+# See https://docs.openshift.com/container-platform/4.8/cicd/pipelines/op-release-notes.html.
+TKN_VERSION="v0.24.0"
 TKN_DASHBOARD_VERSION="v0.17.0"
-TKN_TRIGGERS="v0.12.0"
+TKN_TRIGGERS="v0.14.0"
 
 INSTALL_TKN_DASHBOARD="false"
 
@@ -33,7 +33,11 @@ esac; shift; done
 if ! $KUBECTL_BIN get namespace tekton-pipelines &> /dev/null; then
     echo "Installing Tekton ..."
     $KUBECTL_BIN apply --filename https://storage.googleapis.com/tekton-releases/pipeline/previous/${TKN_VERSION}/release.notags.yaml
+
+    echo "Installing Tekton Triggers ..."
     $KUBECTL_BIN apply --filename https://storage.googleapis.com/tekton-releases/triggers/previous/${TKN_TRIGGERS}/release.yaml
+    $KUBECTL_BIN apply --filename https://storage.googleapis.com/tekton-releases/triggers/previous/${TKN_TRIGGERS}/interceptors.yaml
+
     if [ "${INSTALL_TKN_DASHBOARD}" != "false" ]; then
         echo "Installing Tekton Dashboard..."
         $KUBECTL_BIN apply --filename https://storage.googleapis.com/tekton-releases/dashboard/previous/${TKN_DASHBOARD_VERSION}/tekton-dashboard-release.yaml
