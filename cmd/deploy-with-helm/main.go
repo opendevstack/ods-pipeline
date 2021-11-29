981156b5f42efc3d5b8e59148462d8eb8bca7e4a
Castillo,Gerard (BI X) BIX-DE-I <gerard.castillo@boehringer-ingelheim.com>
2021-11-29 07:45:45 +0100

    taks ods deploy helm using age encryption - removing pgp dependency

--- cmd/deploy-with-helm/main.go
+++ cmd/deploy-with-helm/main.go
@@ -10,7 +10,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -21,7 +20,6 @@ import (
 	"github.com/opendevstack/pipeline/pkg/artifact"
 	"github.com/opendevstack/pipeline/pkg/config"
 	"github.com/opendevstack/pipeline/pkg/pipelinectxt"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/yaml"
@@ -49,21 +47,17 @@ Error: plugin "diff" exited with error
 Error: plugin "secrets" exited with error`
 
 type options struct {
-	chartDir              string
-	releaseName           string
-	privateKeySecret      string
-	privateKeySecretField string
-	certDir               string
-	srcRegistryTLSVerify  bool
-	debug                 bool
+	chartDir             string
+	releaseName          string
+	certDir              string
+	srcRegistryTLSVerify bool
+	debug                bool
 }
 
 func main() {
 	opts := options{}
 	flag.StringVar(&opts.chartDir, "chart-dir", "", "Chart dir")
 	flag.StringVar(&opts.releaseName, "release-name", "", "release-name")
-	flag.StringVar(&opts.privateKeySecret, "private-key-secret", "", "Name of the secret containing the PGP private key to use for helm-secrets")
-	flag.StringVar(&opts.privateKeySecretField, "private-key-secret-field", "sops.asc", "Name of the field in the secret holding the private key")
 	flag.StringVar(&opts.certDir, "cert-dir", "/etc/containers/certs.d", "Use certificates at the specified path to access the registry")
 	flag.BoolVar(&opts.srcRegistryTLSVerify, "src-registry-tls-verify", true, "TLS verify source registry")
 	flag.BoolVar(&opts.debug, "debug", (os.Getenv("DEBUG") == "true"), "debug mode")
@@ -341,24 +335,7 @@ func main() {
 		}
 	}
 
-	if len(opts.privateKeySecret) == 0 {
-		fmt.Println("Skipping import of private key for helm-secrets as parameter is not set ...")
-	} else {
-		fmt.Println("Importing private key for helm-secrets ...")
-		secret, err := clientset.CoreV1().Secrets(ctxt.Namespace).Get(
-			context.TODO(), opts.privateKeySecret, metav1.GetOptions{},
-		)
-		if err != nil {
-			fmt.Printf("No secret %s found, skipping.\n", opts.privateKeySecret)
-		} else {
-			stdout, stderr, err = importPrivateKey(secret, opts.privateKeySecretField)
-			if err != nil {
-				fmt.Println(string(stderr))
-				log.Fatal(err)
-			}
-			fmt.Println(string(stdout))
-		}
-	}
+	// TODO add a check for age-key-secret
 
 	fmt.Printf("Diffing Helm release against %s...\n", helmArchive)
 	helmDiffArgs := []string{
@@ -473,22 +450,6 @@ func writeDeploymentArtifact(content []byte, filename, chartDir, targetEnv strin
 	}
 	f := artifactFilename(filename, chartDir, targetEnv) + ".txt"
 	return ioutil.WriteFile(filepath.Join(pipelinectxt.DeploymentsPath, f), content, 0644)
-}
-
-func importPrivateKey(secret *corev1.Secret, privateKeySecretField string) (outBytes, errBytes []byte, err error) {
-	gpgImport := exec.Command("gpg", "--import")
-	var stdout, stderr, stdin bytes.Buffer
-	_, err = stdin.Write(secret.Data[privateKeySecretField])
-	if err != nil {
-		return outBytes, errBytes, err
-	}
-	gpgImport.Stdout = &stdout
-	gpgImport.Stdin = &stdin
-	gpgImport.Stderr = &stderr
-	err = gpgImport.Run()
-	outBytes = stdout.Bytes()
-	errBytes = stderr.Bytes()
-	return outBytes, errBytes, err
 }
 
 func runHelmCmd(args []string, targetConfig *config.Environment, debug bool) (outBytes, errBytes []byte, err error) {
