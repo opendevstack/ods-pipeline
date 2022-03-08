24e015f2de1aff2dd21862ec480956d8c01017bf
Kuebler,Andreas (BI X) BIX-DE-I <andreas.kuebler@boehringer-ingelheim.com>
2022-03-08 14:10:09 +0100

    Remove sidecars, allow for resource quotas and move sonar step to separate template

--- test/tasks/common_test.go
+++ test/tasks/common_test.go
@@ -3,7 +3,6 @@ package tasks
 import (
 	"crypto/sha256"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -150,9 +149,6 @@ func runTaskTestCases(t *testing.T, taskName string, requiredServices []tasktest
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
 			tn := taskName
-			if tc.TaskVariant != "" {
-				tn = fmt.Sprintf("%s-%s", taskName, tc.TaskVariant)
-			}
 			if tc.Timeout == 0 {
 				tc.Timeout = 5 * time.Minute
 			}
