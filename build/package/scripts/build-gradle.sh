ba539852e5e7a0de182d79cd8b4c71b4ea612e6a
René Dupont <rene.dupont@boehringer-ingelheim.com>
2022-04-01 15:58:59 +0200

    improve code comment

--- build/package/scripts/build-gradle.sh
+++ build/package/scripts/build-gradle.sh
@@ -73,8 +73,8 @@ UNIT_TEST_RESULT_DIR="${BUILD_DIR}/test-results/test"
 if [ -d "${UNIT_TEST_RESULT_DIR}" ]; then
     UNIT_TEST_ARTIFACTS_DIR="${ARTIFACTS_DIR}/xunit-reports"
     mkdir -p "${UNIT_TEST_ARTIFACTS_DIR}"
-    # Each test class produces its own report file, but they contain a qualified package
-    # name in their file name. That way we do not need to add an artifact prefix to
+    # Each test class produces its own report file, but they contain a fully qualified class
+    # name in their file name. Due to that, we do not need to add an artifact prefix to
     # distinguish them with reports from other artifacts of the same repo/pipeline build.
     cp "${UNIT_TEST_RESULT_DIR}/"*.xml "${UNIT_TEST_ARTIFACTS_DIR}"
 else
