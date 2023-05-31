5dd5242ba5a5c413af2c73b7dedb0d505f08cc18
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2023-05-31 14:45:38 +0200

    Take working-dir param into account when supplying SQ default properties
    Fixes #674

--- build/package/scripts/supply-sonar-project-properties-default.sh
+++ build/package/scripts/supply-sonar-project-properties-default.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -eu
 
+working_dir="."
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    --working-dir) working_dir="$2"; shift;;
+    --working-dir=*) working_dir="${1#*=}";;
+  *) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
+
 echo "Checking for sonar-project.properties ..."
-if [ ! -f sonar-project.properties ]; then
+if [ ! -f "${working_dir}/sonar-project.properties" ]; then
   echo "No sonar-project.properties present, using default:"
   cat /usr/local/default-sonar-project.properties
-  cp /usr/local/default-sonar-project.properties sonar-project.properties
+  cp /usr/local/default-sonar-project.properties "${working_dir}/sonar-project.properties"
 fi
