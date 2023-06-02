16b41b1e372336e14dbd01f4e0a4ad7114bdd0a7
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2023-06-02 14:00:56 +0200

    Fix artifact and output caching of Gradle task
    * Change working dir to the one specified by the `working-dir` param
    * Place artifacts into `.ods/tmp-artifacts` so that they are properly
      cached
    * Make Gradle build dir configurable instead of hardcoding to `build`
    
    Fixes #700.
    Fixes #702.

--- build/package/scripts/build-gradle.sh
+++ build/package/scripts/build-gradle.sh
@@ -1,55 +1,66 @@
 #!/bin/bash
 set -eu
 
-OUTPUT_DIR="docker"
-WORKING_DIR="."
-ROOT_DIR=$(pwd)
-export ARTIFACTS_DIR=$ROOT_DIR/.ods/artifacts
-ARTIFACT_PREFIX=
-DEBUG="${DEBUG:-false}"
-GRADLE_ADDITIONAL_TASKS=
-GRADLE_OPTIONS=
+# the copy commands are based on GNU cp tools
+# On a mac `brew install coreutils` gives `g` prefixed cmd line tools such as gcp
+# to use these define env variable GNU_CP=gcp before invoking this script.
+CP="${GNU_CP:-cp}"
+
+output_dir="docker"
+working_dir="."
+artifact_prefix=""
+debug="${DEBUG:-false}"
+gradle_build_dir="build"
+gradle_additional_tasks=
+gradle_options=
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
 
-    --working-dir) WORKING_DIR="$2"; shift;;
-    --working-dir=*) WORKING_DIR="${1#*=}";;
+    --working-dir) working_dir="$2"; shift;;
+    --working-dir=*) working_dir="${1#*=}";;
 
-    --output-dir) OUTPUT_DIR="$2"; shift;;
-    --output-dir=*) OUTPUT_DIR="${1#*=}";;
+    --output-dir) output_dir="$2"; shift;;
+    --output-dir=*) output_dir="${1#*=}";;
 
-    --gradle-additional-tasks) GRADLE_ADDITIONAL_TASKS="$2"; shift;;
-    --gradle-additional-tasks=*) GRADLE_ADDITIONAL_TASKS="${1#*=}";;
+    --gradle-build-dir) gradle_build_dir="$2"; shift;;
+    --gradle-build-dir=*) gradle_build_dir="${1#*=}";;
+
+    --gradle-additional-tasks) gradle_additional_tasks="$2"; shift;;
+    --gradle-additional-tasks=*) gradle_additional_tasks="${1#*=}";;
 
     # Gradle project properties ref: https://docs.gradle.org/7.4.2/userguide/build_environment.html#sec:gradle_configuration_properties
     # Gradle options ref: https://docs.gradle.org/7.4.2/userguide/command_line_interface.html
-    --gradle-options) GRADLE_OPTIONS="$2"; shift;;
-    --gradle-options=*) GRADLE_OPTIONS="${1#*=}";;
+    --gradle-options) gradle_options="$2"; shift;;
+    --gradle-options=*) gradle_options="${1#*=}";;
 
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
 
-if [ "${WORKING_DIR}" != "." ]; then
-  WORKING_DIR="${ROOT_DIR}"
-  ARTIFACT_PREFIX="${WORKING_DIR/\//-}-"
+root_dir=$(pwd)
+tmp_artifacts_dir="${root_dir}/.ods/tmp-artifacts"
+# tmp_artifacts_dir enables keeping artifacts created by this build
+# separate from other builds in the same repo to facilitate caching.
+rm -rf "${tmp_artifacts_dir}"
+if [ "${working_dir}" != "." ]; then
+  cd "${working_dir}"
+  artifact_prefix="${working_dir/\//-}-"
 fi
 
-if [ "${DEBUG}" == "true" ]; then
+if [ "${debug}" == "true" ]; then
   set -x
 fi
 
 echo "Using NEXUS_URL=$NEXUS_URL"
 echo "Using GRADLE_OPTS=$GRADLE_OPTS"
 echo "Using GRADLE_USER_HOME=$GRADLE_USER_HOME"
-echo "Using ARTIFACTS_DIR=$ARTIFACTS_DIR"
 mkdir -p "${GRADLE_USER_HOME}"
 
 configure-gradle
 
 echo
-cd "${WORKING_DIR}"
-echo "Working on Gradle project in '${WORKING_DIR}'..."
+cd "${working_dir}"
+echo "Working on Gradle project in '${working_dir}'..."
 echo
 echo "Gradlew version: "
 ./gradlew -version
@@ -61,37 +72,35 @@ echo " that this build expects generated application artifacts to be copied to."
 echo " The project gradle script should read this env var to copy all the "
 echo " generated application artifacts."
 echo
-export ODS_OUTPUT_DIR=${OUTPUT_DIR}
-echo "Exported env var 'ODS_OUTPUT_DIR' with value '${OUTPUT_DIR}'"
+export ODS_OUTPUT_DIR=${output_dir}
+echo "Exported env var 'ODS_OUTPUT_DIR' with value '${output_dir}'"
 echo
 echo "Building (Compile and Test) ..."
 # shellcheck disable=SC2086
-./gradlew clean build ${GRADLE_ADDITIONAL_TASKS} ${GRADLE_OPTIONS}
+./gradlew clean build ${gradle_additional_tasks} ${gradle_options}
 echo
 
 echo "Verifying unit test report was generated  ..."
-BUILD_DIR="build"
-UNIT_TEST_RESULT_DIR="${BUILD_DIR}/test-results/test"
-
-if [ -d "${UNIT_TEST_RESULT_DIR}" ]; then
-    UNIT_TEST_ARTIFACTS_DIR="${ARTIFACTS_DIR}/xunit-reports"
-    mkdir -p "${UNIT_TEST_ARTIFACTS_DIR}"
+unit_test_result_dir="${gradle_build_dir}/test-results/test"
+if [ -d "${unit_test_result_dir}" ]; then
+    unit_test_artifacts_dir="${tmp_artifacts_dir}/xunit-reports"
+    mkdir -p "${unit_test_artifacts_dir}"
     # Each test class produces its own report file, but they contain a fully qualified class
     # name in their file name. Due to that, we do not need to add an artifact prefix to
     # distinguish them with reports from other artifacts of the same repo/pipeline build.
-    cp "${UNIT_TEST_RESULT_DIR}/"*.xml "${UNIT_TEST_ARTIFACTS_DIR}"
+    "$CP" "${unit_test_result_dir}/"*.xml "${unit_test_artifacts_dir}"
 else
-  echo "Build failed: no unit test results found in ${UNIT_TEST_RESULT_DIR}"
+  echo "Build failed: no unit test results found in ${unit_test_result_dir}"
   exit 1
 fi
 
 echo "Verifying unit test coverage report was generated  ..."
-COVERAGE_RESULT_DIR="${BUILD_DIR}/reports/jacoco/test"
-if [ -d "${COVERAGE_RESULT_DIR}" ]; then
-    CODE_COVERAGE_ARTIFACTS_DIR="${ARTIFACTS_DIR}/code-coverage"
-    mkdir -p "${CODE_COVERAGE_ARTIFACTS_DIR}"
-    cp "${COVERAGE_RESULT_DIR}/jacocoTestReport.xml" "${CODE_COVERAGE_ARTIFACTS_DIR}/${ARTIFACT_PREFIX}coverage.xml"
+coverage_result_dir="${gradle_build_dir}/reports/jacoco/test"
+if [ -d "${coverage_result_dir}" ]; then
+    code_coverage_artifacts_dir="${tmp_artifacts_dir}/code-coverage"
+    mkdir -p "${code_coverage_artifacts_dir}"
+    "$CP" "${coverage_result_dir}/jacocoTestReport.xml" "${code_coverage_artifacts_dir}/${artifact_prefix}coverage.xml"
 else
-  echo "Build failed: no unit test coverage report was found in ${COVERAGE_RESULT_DIR}"
+  echo "Build failed: no unit test coverage report was found in ${coverage_result_dir}"
   exit 1
 fi
